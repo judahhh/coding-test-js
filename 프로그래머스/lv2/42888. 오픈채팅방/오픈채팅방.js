e450decb1d85f007b5c1bd0632290c484a41ed8e
judahhh <jdh99080516@gmail.com>
2023-02-20 13:12:22 +0900

    [level 2] Title: 오픈채팅방, Time: 146.82 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,18 @@
+function solution(record) {
+    let answer = [],obj={}
+  record.forEach(v=>{
+let [type,id,name]=v.split(" ")
+if(type!="Leave")
+obj[id]=name
+
+  })
+
+record.forEach(v=>{
+let [type,id,name]=v.split(" ")
+if(type=="Enter")
+answer.push(`${obj[id]}님이 들어왔습니다.`)
+if(type=="Leave")
+answer.push(`${obj[id]}님이 나갔습니다.`)
+})
+    return answer
+}
