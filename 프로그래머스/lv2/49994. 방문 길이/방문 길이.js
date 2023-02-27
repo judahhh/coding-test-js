d027b4d0a1dfd09e70ea87f991334fd77bbc40a0
judahhh <jdh99080516@gmail.com>
2023-02-27 15:06:05 +0900

    [level 2] Title: 방문 길이, Time: 0.78 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,22 +1,23 @@
 function solution(dirs) {
     let answer = 0,coordinate=[],now=[0,0]
-dirs=dirs.split("")
-    dirs.forEach(v=>{
-let before=[now[0],now[1]]
+   
+    dirs.split('').forEach(v=>{
 
-if(now[1]!==5&&v=='U') now[1]++
-else if(now[1]!==-5&&v=='D') now[1]--
-else if(now[0]!==-5&&v=='L') now[0]--
-else if(now[0]!==5&&v=='R') now[0]++
+     let before=[now[0],now[1]]
 
-if((before[0]!==now[0]||before[1]!==now[1])&&
-coordinate.includes(`${before[0]},${before[1]},${now[0]},${now[1]}`)==false&&
-coordinate.includes(`${now[0]},${now[1]},${before[0]},${before[1]}`)==false
-){
-coordinate.push(`${before[0]},${before[1]},${now[0]},${now[1]}`)
-answer++
-}
-//console.log(coordinate)
-})
+      if(now[1]!==5&&v=='U') now[1]++
+      else if(now[1]!==-5&&v=='D') now[1]--
+      else if(now[0]!==-5&&v=='L') now[0]--
+      else if(now[0]!==5&&v=='R') now[0]++
+
+      if((before[0]!==now[0]||before[1]!==now[1])&&
+          coordinate.includes(`${before[0]},${before[1]},${now[0]},${now[1]}`)==false&&
+          coordinate.includes(`${now[0]},${now[1]},${before[0]},${before[1]}`)==false
+          ){ 
+          coordinate.push(`${before[0]},${before[1]},${now[0]},${now[1]}`)
+          answer++
+          }
+
+    })
     return answer
 }
