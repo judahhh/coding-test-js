c7280f881babc3bcbaa1ab4d42c160596d1c073c
judahhh <jdh99080516@gmail.com>
2023-04-23 13:52:31 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,21 @@
+function solution(n, computers) {
+    let answer = 0,visited=Array(n).fill(false)
+
+for(let i=0;i<n;i++){
+ if(!visited[i]){
+ dfs(i)
+ answer++
+ }
+}
+function dfs(i){
+visited[i]=true
+for(let j=0;j<n;j++){
+ if(computers[i][j]===1&& !visited[j]){
+ dfs(j)
+
+ }
+ }
+ }
+
+    return answer
+}
