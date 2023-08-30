9642bdd35f2b9b29c981a930f2d3af5a77243f17
DaHyeonJu <jdh99080516@gmail.com>
2023-08-30 14:03:04 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,29 +1,23 @@
-function solution(n, k) {
-    let answer = 0,prime,stack=[],arr=[]
-prime=(n.toString(k)+"").split("")
-for(let i=0;i<prime.length;i++){
-  if(prime[i]!=="0")
-   stack.push(prime[i])
-  if(prime[i]==0&&i!==prime.length-1&&stack.length!=0) {
-  arr.push(stack.join("")) 
-   stack=[]
-   continue
-  }
- if(i==prime.length-1&&stack.length!=0)arr.push(stack.join("")) 
+function isPrime(num){
+if(num<2) return false
+ for(let i=2;i<=Math.sqrt(num);i++){
+ if(num%i===0) return false
+ }
+return true
 }
 
-for(let i=0;i<arr.length;i++){
-if(arr[i]!=1&&isPrime(arr[i]))
-answer++
-}
-return answer
-}
+function solution (n,k){
+  let answer=[], result=[]
+  let arr=n.toString(k).split('')
 
-function isPrime(num) {
-  if(num === 2) return true
-  for(let i=2;i<=Math.floor(Math.sqrt(num));i++){
-    if(num % i === 0)
-      return false
-  }
-  return true
+arr.forEach((v,i)=> {
+if(v!=='0') result.push(v)
+else {
+answer.push(result.join(''))
+result=[]
+}
+if(i==arr.length-1&&result.length!==0) answer.push(result.join(''))
+})
+answer=answer.map((v)=>Number(v)).filter(v=>isPrime(v)==true)
+  return answer.length
 }
